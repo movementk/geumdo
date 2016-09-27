15f7c39d50efed05a9036d71e421432145063280
kyh5392 <kyh90495392@gmail.com>
2016-09-27 19:36:49 +0900

    main 작업중..

---
+++ index.php
@@ -0,0 +1,401 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>금도건설</title>
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/default.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+    <body>
+        <a href="#gnb" class="skip-nav sr-only sr-only-focusable">주메뉴로 건너뛰기</a>
+        <a href="#content" class="skip-nav sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
+        <header id="header">
+            <div class="top-menu">
+                <ul>
+                    <li>
+                        <a href="#">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#">SITEMAP</a>
+                    </li>
+                    <li>
+                        <a href="#">CONTACT US</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">협력업체시스템</a>
+                    </li>
+                </ul>
+            </div>
+            <nav id="gnb" tabindex="-1">
+                <div class="wrapper">
+                    <div class="container">
+                        <p class="main-logo">
+                            <a href="/">
+                                <img src="/assets/images/main/logo.jpg" alt="금도건설 로고">
+                            </a>
+                        </p>
+                        <ul>
+                            <li>
+                                <a href="#">회사소개</a>
+                                <ul>
+                                    <li><a href="#">회사소개</a></li>
+                                    <li><a href="#">CEO인사말</a></li>
+                                    <li><a href="#">회사연혁</a></li>
+                                    <li><a href="#">운영조직</a></li>
+                                    <li><a href="#">찾아오시는길</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">사업분야</a>
+                                <ul>
+                                    <li><a href="#">사업영역</a></li>
+                                    <li><a href="#">토목사업</a></li>
+                                    <li><a href="#">건축사업</a></li>
+                                    <li><a href="#">민자사업</a></li>
+                                    <li><a href="#">주택사업</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">홍보센터</a>
+                                <ul>
+                                    <li><a href="#">CI소개</a></li>
+                                    <li><a href="#">R&amp;D센터</a></li>
+                                    <li><a href="#">인증 및 상호</a></li>
+                                    <li><a href="#">관계사 현황</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">채용정보</a>
+                                <ul>
+                                    <li><a href="#">인재상</a></li>
+                                    <li><a href="#">복리후생</a></li>
+                                    <li><a href="#">채용정보</a></li>
+                                    <li><a href="#">이력서등록</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">고객센터</a>
+                                <ul>
+                                    <li><a href="#">공지사항</a></li>
+                                    <li><a href="#">사내소식</a></li>
+                                    <li><a href="#">건설뉴스</a></li>
+                                    <li><a href="#">보도자료</a></li>
+                                    <li><a href="#">고객문의</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main id="content" tabindex="-1">
+            <div id="visual">
+                <ul>
+                    <li>
+                        <div class="container">
+                            <div class="project">
+                                <h2>PROJECT 1</h2>
+                                <h3>
+                                    <small>솔리힐 더 테라스</small><br>
+                                    주택사업부
+                                </h3>
+                                <ul>
+                                    <li>Construction date</li>
+                                    <li>2013~2014</li>
+                                </ul>
+                                <p class="btn-view">
+                                    <a href="#" class="btn" role="button">VIEW</a>
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="container">
+                            <div class="project">
+                                <h2>PROJECT 2</h2>
+                                <h3>
+                                    <small>솔리힐 더 테라스</small><br>
+                                    주택사업부
+                                </h3>
+                                <ul>
+                                    <li>Construction date</li>
+                                    <li>2013~2014</li>
+                                </ul>
+                                <p class="btn-view">
+                                    <a href="#" class="btn" role="button">VIEW</a>
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="container">
+                            <div class="project">
+                                <h2>PROJECT 3</h2>
+                                <h3>
+                                    <small>솔리힐 더 테라스</small><br>
+                                    주택사업부
+                                </h3>
+                                <ul>
+                                    <li>Construction date</li>
+                                    <li>2013~2014</li>
+                                </ul>
+                                <p class="btn-view">
+                                    <a href="#" class="btn" role="button">VIEW</a>
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="business">
+                <div class="container">
+                    <h2>BUSINESS AREA</h2>
+                    <hr>
+                    <ul class="row">
+                        <li class="col-md-12 col-lg-6">
+                            <a href="#">
+                                <h3>토목사업<br><small>CIVIL WORKD</small></h3>
+                            </a>
+                        </li>
+                        <li class="col-md-12 col-lg-6">
+                            <a href="#">
+                                <h3>건축사업<br><small>BUILDNG WORKS</small></h3>
+                            </a>
+                        </li>
+                        <li class="col-md-12 col-lg-6">
+                            <a href="#">
+                                <h3>민자사업<br><small>BUILD-TRANSFER-LEASE</small></h3>
+                            </a>
+                        </li>
+                        <li class="col-md-12 col-lg-6">
+                            <a href="#">
+                                <h3>주택사업<br><small>DEVELOPMENT WORK</small></h3>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="news">
+                <div class="container">
+                    <h2>NEWS</h2>
+                    <hr>
+                    <ul class="row">
+                        <li class="col-md-12 col-lg-4">
+                            <div class="item">
+                                <p>
+                                    <img src="/assets/images/main/news_img01.jpg" class="img-responsive" alt="뉴스 이미지">
+                                </p>
+                                <span>사내<br>소식</span>
+                            </div>
+                            <div class="details">
+                                <a href="#">
+                                    <h4>소통과 리더십을 통한 업무메뉴얼[교육]</h4>
+                                    <p>
+                                        금도건설과 CJ대한통운이 업무협약을 맺었습니다<br>
+                                        금도건설과 CJ대한통운이 업무협약을
+                                    </p>
+                                    <span class="date">2016-07-25</span>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="col-md-12 col-lg-4">
+                            <div class="item">
+                                <p>
+                                    <img src="/assets/images/main/news_img02.jpg" class="img-responsive" alt="뉴스 이미지">
+                                </p>
+                                <span>사내<br>소식</span>
+                            </div>
+                            <div class="details">
+                                <a href="#">
+                                    <h4>건설현장 물류서비스 업무협약(MOU체결)</h4>
+                                    <p>
+                                        금도건설과 CJ대한통운이 업무협약을 맺었습니다<br>
+                                        금도건설과 CJ대한통운이 업무협약을
+                                    </p>
+                                    <span class="date">2016-07-25</span>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="col-md-12 col-lg-4">
+                            <div class="notice-info">
+                                <h4>공지사항 <a href="#"><span class="more"></span></a></h4>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <h5 class="title">근무기강확립 실천사항</h5>
+                                            <span class="date">2016-07-25</span>
+                                            <p>
+                                                2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
+                                                새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
+                                            </p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <h5 class="title">[알림] 당사 그룹웨어 시스템 실천사항..실천사항..</h5>
+                                            <span class="date">2016-07-25</span>
+                                            <p>
+                                                2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
+                                                새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
+                                            </p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <h5 class="title">근무기강확립 실천사항</h5>
+                                            <span class="date">2016-07-25</span>
+                                            <p>
+                                                2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
+                                                새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
+                                            </p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="the-terrace">
+                <ul class="row">
+                    <li class="col-md-12 col-lg-6"></li>
+                    <li class="col-md-12 col-lg-6">
+                        <h3>김포 고촌 솔리힐 더테라스</h3>
+                        <hr>
+                        <p>
+                            산림에서 얻은 자연의 소재를 활용하여 부드럽고 자연스러운 단지 연출과<br>
+                            시간이 지날수록 가치를 더하는 자연과 같은 경관계획을 통해 아름답고 풍요로운<br>
+                            공간을 계획하고자 합니다.
+                        </p>
+                        <p class="more-btn">
+                            <a href="#" class="btn" role="button">자세히보기</a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div class="partner">
+                <div class="container">
+                    <h2>PARTNER</h2>
+                    <hr>
+                    <ul class="slider">
+                        <li>
+                            <a href="#">
+                                <img src="/assets/images/main/partner_img01.jpg" alt="협력사 로고">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/assets/images/main/partner_img02.jpg" alt="협력사 로고">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/assets/images/main/partner_img03.jpg" alt="협력사 로고">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/assets/images/main/partner_img04.jpg" alt="협력사 로고">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/assets/images/main/partner_img05.jpg" alt="협력사 로고">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+        <footer id="footer">
+            <div class="container">
+                <div class="introduce">
+                    <ul>
+                        <li>
+                            <h4><a href="#">회사소개</a></h4>
+                            <ul>
+                                <li><a href="#">회사개요</a></li>
+                                <li><a href="#">CEO인사말</a></li>
+                                <li><a href="#">회사연혁</a></li>
+                                <li><a href="#">운영조직</a></li>
+                                <li><a href="#">찾아오시는길</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h4><a href="#">사업분야</a></h4>
+                            <ul>
+                                <li><a href="#">사업영역</a></li>
+                                <li><a href="#">토목사업</a></li>
+                                <li><a href="#">건축사업</a></li>
+                                <li><a href="#">민자사업</a></li>
+                                <li><a href="#">주택사업</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h4><a href="#">홍보센터</a></h4>
+                            <ul>
+                                <li><a href="#">CI소개</a></li>
+                                <li><a href="#">R&amp;D센터</a></li>
+                                <li><a href="#">인증 및 상호</a></li>
+                                <li><a href="#">관계사 현황</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h4><a href="#">채용정보</a></h4>
+                            <ul>
+                                <li><a href="#">인재상</a></li>
+                                <li><a href="#">복리후생</a></li>
+                                <li><a href="#">채용정보</a></li>
+                                <li><a href="#">이력서등록</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h4><a href="#">고객센터</a></h4>
+                            <ul>
+                                <li><a href="#">공지사항</a></li>
+                                <li><a href="#">사내소식</a></li>
+                                <li><a href="#">건설뉴스</a></li>
+                                <li><a href="#">보도자료</a></li>
+                                <li><a href="#">고객문의</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="company">
+
+                </div>
+            </div>
+        </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/jquery.bxslider.min.js"></script>
+    <script>
+        (function($) {
+            $(document).on('mouseenter focusin', '#gnb .container > ul', function() {
+                $('#gnb').addClass('opened');
+            });
+            $(document).on('mouseleave', '#gnb .container > ul', function() {
+                $('#gnb').removeClass('opened');
+            });
+        })(jQuery);
+        (function($) {
+            $('#visual > ul').bxSlider();
+        })(jQuery);
+
+        var partnerSlider = $('.partner .slider').bxSlider({
+            minSlides: 5,
+            maxSlides: 5,
+            slideWidth: 170,
+            slideMargin: 30,
+            pager: false,
+            controls: true
+        });
+    </script>
+    </body>
+</html>
