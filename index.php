9d84470d10f78b850299655c9baed5786fcf7544
kyh5392 <kyh90495392@gmail.com>
2016-09-28 19:14:51 +0900

    메인 퍼블리싱 작업중 [web] 작업 완료 #1

--- index.php
+++ index.php
@@ -161,21 +161,49 @@
                         <li class="col-md-12 col-lg-6">
                             <a href="#">
                                 <h3>토목사업<br><small>CIVIL WORKD</small></h3>
+                                <div class="hover-details">
+                                    <h4 class="hover-txt">토목사업</h4>
+                                    <p>
+                                        금도건설은 앞으로도 친환경적이며 과학적인 건설을 통해 인간과 자연이 함께하는<br>
+                                        시설물의시공에 앞장서겠습니다.
+                                    </p>
+                                </div>
                             </a>
                         </li>
                         <li class="col-md-12 col-lg-6">
                             <a href="#">
                                 <h3>건축사업<br><small>BUILDNG WORKS</small></h3>
+                                <div class="hover-details">
+                                    <h4 class="hover-txt">건축사업</h4>
+                                    <p>
+                                        삶의 질 향상과 주거문화 개선에 앞장서며 보다 살기좋은 세상을 구현하는데 최선의 노력을 다하는 <br>
+                                        금도건설이 되겠습니다.
+                                    </p>
+                                </div>
                             </a>
                         </li>
                         <li class="col-md-12 col-lg-6">
                             <a href="#">
                                 <h3>민자사업<br><small>BUILD-TRANSFER-LEASE</small></h3>
+                                <div class="hover-details">
+                                    <h4 class="hover-txt">민자사업</h4>
+                                    <p>
+                                        금도건설은 우수한 인력과 신기술, 신공법, 실용신안 등의 건설기술개발을 통한 차별화된<br>
+                                        기술축적을 통하여 건설 발전에 한걸음 앞으로 나아가겠습니다.
+                                    </p>
+                                </div>
                             </a>
                         </li>
                         <li class="col-md-12 col-lg-6">
                             <a href="#">
                                 <h3>주택사업<br><small>DEVELOPMENT WORK</small></h3>
+                                <div class="hover-details">
+                                    <h4 class="hover-txt">주택사업</h4>
+                                    <p>
+                                        금도건설은 열정적인 자세와 혁신을 통한 꾸준한 가치창조에 끊임없이 도전하며,<br>
+                                        세계일류의 NAME VALUE 실현을 목표로 더욱더 노력하겠습니다.
+                                    </p>
+                                </div>
                             </a>
                         </li>
                     </ul>
@@ -187,76 +215,82 @@
                     <hr>
                     <ul class="row">
                         <li class="col-md-12 col-lg-4">
-                            <div class="item">
-                                <p>
-                                    <img src="/assets/images/main/news_img01.jpg" class="img-responsive" alt="뉴스 이미지">
-                                </p>
-                                <span>사내<br>소식</span>
-                            </div>
-                            <div class="details">
-                                <a href="#">
-                                    <h4>소통과 리더십을 통한 업무메뉴얼[교육]</h4>
+                            <div class="hover-box">
+                                <div class="item">
                                     <p>
-                                        금도건설과 CJ대한통운이 업무협약을 맺었습니다<br>
-                                        금도건설과 CJ대한통운이 업무협약을
+                                        <img src="/assets/images/main/news_img01.jpg" class="img-responsive" alt="뉴스 이미지">
                                     </p>
-                                    <span class="date">2016-07-25</span>
-                                </a>
+                                    <span>사내<br>소식</span>
+                                </div>
+                                <div class="details">
+                                    <a href="#">
+                                        <h4>소통과 리더십을 통한 업무메뉴얼[교육]</h4>
+                                        <p>
+                                            금도건설과 CJ대한통운이 업무협약을 맺었습니다<br>
+                                            금도건설과 CJ대한통운이 업무협약을
+                                        </p>
+                                        <span class="date">2016-07-25</span>
+                                    </a>
+                                </div>
                             </div>
                         </li>
                         <li class="col-md-12 col-lg-4">
-                            <div class="item">
-                                <p>
-                                    <img src="/assets/images/main/news_img02.jpg" class="img-responsive" alt="뉴스 이미지">
-                                </p>
-                                <span>사내<br>소식</span>
-                            </div>
-                            <div class="details">
-                                <a href="#">
-                                    <h4>건설현장 물류서비스 업무협약(MOU체결)</h4>
+                            <div class="hover-box">
+                                <div class="item">
                                     <p>
-                                        금도건설과 CJ대한통운이 업무협약을 맺었습니다<br>
-                                        금도건설과 CJ대한통운이 업무협약을
+                                        <img src="/assets/images/main/news_img02.jpg" class="img-responsive" alt="뉴스 이미지">
                                     </p>
-                                    <span class="date">2016-07-25</span>
-                                </a>
+                                    <span>사내<br>소식</span>
+                                </div>
+                                <div class="details">
+                                    <a href="#">
+                                        <h4>건설현장 물류서비스 업무협약(MOU체결)</h4>
+                                        <p>
+                                            금도건설과 CJ대한통운이 업무협약을 맺었습니다<br>
+                                            금도건설과 CJ대한통운이 업무협약을
+                                        </p>
+                                        <span class="date">2016-07-25</span>
+                                    </a>
+                                </div>
                             </div>
                         </li>
                         <li class="col-md-12 col-lg-4">
-                            <div class="notice-info">
-                                <h4>공지사항 <a href="#"><span class="more"></span></a></h4>
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <h5 class="title">근무기강확립 실천사항</h5>
-                                            <span class="date">2016-07-25</span>
-                                            <p>
-                                                2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
-                                                새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
-                                            </p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <h5 class="title">[알림] 당사 그룹웨어 시스템 실천사항..실천사항..</h5>
-                                            <span class="date">2016-07-25</span>
-                                            <p>
-                                                2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
-                                                새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
-                                            </p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <h5 class="title">근무기강확립 실천사항</h5>
-                                            <span class="date">2016-07-25</span>
-                                            <p>
-                                                2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
-                                                새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
+                            <div class="hover-box">
+                                <div class="notice-info">
+                                    <h4>공지사항 <a href="#"><span class="more"></span></a></h4>
+                                    <ul>
+                                        <li>
+                                            <a href="#">
+                                                <h5 class="title">근무기강확립 실천사항</h5>
+                                                <span class="date">2016-07-25</span>
+                                                <p>
+                                                    2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
+                                                    새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
+                                                </p>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <h5 class="title">[알림] 당사 그룹웨어 시스템 실천사항..실천사항..</h5>
+                                                <span class="date">2016-07-25</span>
+                                                <p>
+                                                    2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
+                                                    새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
+                                                </p>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <h5 class="title">근무기강확립 실천사항</h5>
+                                                <span class="date">2016-07-25</span>
+                                                <p>
+                                                    2014년 새롭게 시작하는 마음으로 어려운 시기를 극복하고<br>
+                                                    새롭게 도약하는 선진기업을 만들기 위해 다음과 같이 근무...
+                                                </p>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </li>
                     </ul>
@@ -284,29 +318,29 @@
                     <h2>PARTNER</h2>
                     <hr>
                     <ul class="slider">
-                        <li>
+                        <li class="partner-1">
                             <a href="#">
-                                <img src="/assets/images/main/partner_img01.jpg" alt="협력사 로고">
+                                <span class="sr-only">한국건설기술인협회 로고</span>
                             </a>
                         </li>
-                        <li>
+                        <li class="partner-2">
                             <a href="#">
-                                <img src="/assets/images/main/partner_img02.jpg" alt="협력사 로고">
+                                <span class="sr-only">건설공제조합 로고</span>
                             </a>
                         </li>
-                        <li>
+                        <li class="partner-3">
                             <a href="#">
-                                <img src="/assets/images/main/partner_img03.jpg" alt="협력사 로고">
+                                <span class="sr-only">국토교통부 로고</span>
                             </a>
                         </li>
-                        <li>
+                        <li class="partner-4">
                             <a href="#">
-                                <img src="/assets/images/main/partner_img04.jpg" alt="협력사 로고">
+                                <span class="sr-only">나라장터 로고</span>
                             </a>
                         </li>
-                        <li>
+                        <li class="partner-5">
                             <a href="#">
-                                <img src="/assets/images/main/partner_img05.jpg" alt="협력사 로고">
+                                <span class="sr-only">대한걸설협회 로고</span>
                             </a>
                         </li>
                     </ul>
@@ -368,7 +402,39 @@
                     </ul>
                 </div>
                 <div class="company">
-
+                    <h2><a href="/"><img src="/assets/images/main/foot_logo.jpg" alt="금도건설 로고"></a></h2>
+                    <dl>
+                        <dt>주소</dt>
+                        <dd>전북 군산시 현충로 35(나운동 1255-1)</dd>
+                    </dl>
+                    <dl>
+                        <dt>전화</dt>
+                        <dd>063) 461-3442</dd>
+                        <dt>팩스</dt>
+                        <dd>063) 462-3204</dd>
+                    </dl>
+                    <ul>
+                        <li><a href="#">이용약관</a></li>
+                        <li><a href="#">개인정보취급방침</a></li>
+                        <li><a href="#">법적고지</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="copyright">
+                <div class="container">
+                    <p>
+                        COPYRIGHT(C) 2016 BY (주)금도건설 ALL RIGHT RESERVED. SITE BY <a href="http://www.movementk.com">MOVEMENT K GROUP.</a>
+                    </p>
+                    <div class="dropup">
+                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            FAMILY SITE
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                            <li><a href="http://www.gdhomerice.co.kr/shop/" target="_blank">GD홈라이스</a></li>
+                            <li><a href="http://www.gdconvention.com/" target="_blank">GD웨딩컨벤션</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </footer>
@@ -385,7 +451,12 @@
             });
         })(jQuery);
         (function($) {
-            $('#visual > ul').bxSlider();
+            $('#visual > ul').bxSlider({
+                auto: true,
+                controls: true,
+                prevText: '<span class="glyphicon glyphicon-menu-left"></span>',
+                nextText: '<span class="glyphicon glyphicon-menu-right"></span>'
+            });
         })(jQuery);
 
         var partnerSlider = $('.partner .slider').bxSlider({
